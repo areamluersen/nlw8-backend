5eb4e7c37102be8f72b6eea3a0d44f252148c48d
Aream Luersen <areamluersen@gmail.com>
2022-05-10 21:36:45 -0300

    feat: added screenshot on email return

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -34,6 +34,7 @@ export class SubmitFeedbackUseCase {
             '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
             `<p>Tipo de feedback: ${type}</p>`, 
             `<p>Comentário: ${comment}</p>`,
+            screenshot ? `<img src="${screenshot}" style="width: 50%;" />` : '',
             '</div>'
             ].join('\n')})
     }
